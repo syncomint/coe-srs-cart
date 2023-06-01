64953ea7055356cc8c9401c6d9ad7bcedf5dae11
Koushik Gurijala Reddy Yachendra <x252565@corp.ads>
2023-06-01 10:37:53 -0400

    Trying PVE

---
+++ sample.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+list2 = [[1,2,3],[4,5,6]]
+array2 = np.array(list2)
+print(array2)
+
+ages = np.array([13,25,19])
+series1 = pd.Series(ages)
+print(series1)
